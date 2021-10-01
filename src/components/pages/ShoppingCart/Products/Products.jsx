89ff9d96fe9c90da3c6eb86c16cb1911190433c9
Dheeraj Varshney <dheerajvarshney@mail.com>
2021-10-02 01:14:40 +0530

    Fixed Layout

--- src/components/pages/ShoppingCart/Products/Products.jsx
+++ src/components/pages/ShoppingCart/Products/Products.jsx
@@ -56,138 +56,138 @@ const Products = ({ products, currency_symbol }) => {
       <div className={style.productList}>
         {products.map((product) => (
           <>
-            <Link to={`product/${getSKuId(product?.sku)}`}>
-              <div key={product.id} className={style.product}>
-                <div className="d-flex justify-content-between p-12px">
-                  <div className={style.productDetails}>
-                    <div className={style.productImg}>
+            <div key={product.id} className={style.product}>
+              <div className="d-flex justify-content-between p-12px">
+                <div className={style.productDetails}>
+                  <div className={style.productImg}>
+                    <Link to={`product/${getSKuId(product?.sku)}`}>
                       <Image
                         src={product.src}
                         width="200px"
                         alt={product.name}
                         type="product-details"
                       />
-                    </div>
-                    <div className="w-100">
-                      <div className="d-flex w-100">
-                        <div className={style.nameClrSize}>
-                          <h2 className={style.name}>{product.name}</h2>
-                          <div className={style.clrSize}>
-                            <div className="d-flex align-items-center my-10px">
-                              <span>Color:</span>
-                              <span className="font-light-black">
-                                {product?.color?.label ||
-                                  getColorSize(
-                                    product?.product_option
-                                      ?.extension_attributes
-                                      ?.configurable_item_options || []
-                                  ).colors?.[0]?.label}
-                              </span>
-                            </div>
-                            <div className="d-flex align-items-center my-10px">
-                              <span>Size:</span>
-                              <span className="font-light-black">
-                                {product?.size?.label ||
-                                  getColorSize(
-                                    product?.product_option
-                                      ?.extension_attributes
-                                      ?.configurable_item_options || []
-                                  ).size?.[0]?.label}
-                              </span>
-                            </div>
-                            <div className="d-flex align-items-center my-10px">
-                              <span>SKU:</span>
-                              <span className="font-light-black">
-                                {product.sku}
-                              </span>
-                            </div>
+                    </Link>
+                  </div>
+                  <div className="w-100">
+                    <div className="d-flex w-100">
+                      <div className={style.nameClrSize}>
+                        <h2 className={style.name}>{product.name}</h2>
+                        <div className={style.clrSize}>
+                          <div className="d-flex align-items-center my-10px">
+                            <span>Color:</span>
+                            <span className="font-light-black">
+                              {product?.color?.label ||
+                                getColorSize(
+                                  product?.product_option
+                                    ?.extension_attributes
+                                    ?.configurable_item_options || []
+                                ).colors?.[0]?.label}
+                            </span>
                           </div>
-                        </div>
-                        <div className={style.productPricing}>
-                          <div className="d-flex align-items-center justify-content-between">
-                            <strong className="f1 text-center">
-                              {currency_symbol}
-                              {product.price}
-                            </strong>
-                            <div className={style.counter}>
-                              <span
-                                className="c-pointer material-icons-outlined"
-                                onClick={() => {
-                                  handleDecrementProduct(product);
-                                }}
-                              >
-                                remove
-                              </span>
-                              <span>{product.qty}</span>
-                              <span
-                                className="c-pointer material-icons-outlined"
-                                onClick={() => {
-                                  handleIncrementProduct(product);
-                                }}
-                              >
-                                add
-                              </span>
-                            </div>
-                            <strong className="f1 text-center">
-                              {currency_symbol}
-                              {product.price * product.qty}
-                            </strong>
+                          <div className="d-flex align-items-center my-10px">
+                            <span>Size:</span>
+                            <span className="font-light-black">
+                              {product?.size?.label ||
+                                getColorSize(
+                                  product?.product_option
+                                    ?.extension_attributes
+                                    ?.configurable_item_options || []
+                                ).size?.[0]?.label}
+                            </span>
                           </div>
-                          <div className={`text-right ${style.loyaltyPoints}`}>
-                            <span>
-                              Earn Loyalty Points: {currency_symbol}2 ?
+                          <div className="d-flex align-items-center my-10px">
+                            <span>SKU:</span>
+                            <span className="font-light-black">
+                              {product.sku}
                             </span>
                           </div>
                         </div>
                       </div>
-                      <div className={style.footer}>
-                        <div className={style.footerContent}>
-                          <div
-                            className="font-light-black c-pointer d-flex justify-content-center"
-                            onClick={() => handleWishList(product)}
-                            style={{
-                              color: wishlist.find((w) => w.id == product.id)
-                                ? "red"
-                                : "black",
-                            }}
-                          >
-                            <span className={`material-icons-outlined ${style.closeHeart}`}>
-                              {wishlist.find((w) => w.id == product.id)
-                                ? "favorite"
-                                : "favorite_border"}
-                            </span>
-                            <span className="underline">Move to Wishlist</span>
-                          </div>
-                          <Link
-                            to={`/product/${product.sku}`}
-                            className="font-light-black c-pointer d-flex"
-                          >
-                            <span className="material-icons-outlined">
-                              edit
-                            </span>
-                            <span className="underline">Edit</span>
-                          </Link>
-                          <br />
-                          <div className={style.removeBtn}>
+                      <div className={style.productPricing}>
+                        <div className="d-flex align-items-center justify-content-between">
+                          <strong className="f1 text-center">
+                            {currency_symbol}
+                            {product.price}
+                          </strong>
+                          <div className={style.counter}>
                             <span
-                              className={`material-icons-outlined ${style.close}`}
+                              className="c-pointer material-icons-outlined"
+                              onClick={() => {
+                                handleDecrementProduct(product);
+                              }}
                             >
-                              close
+                              remove
                             </span>
+                            <span>{product.qty}</span>
                             <span
-                              onClick={() => removeProduct(product)}
-                              className={style.remove}
+                              className="c-pointer material-icons-outlined"
+                              onClick={() => {
+                                handleIncrementProduct(product);
+                              }}
                             >
-                              Remove
+                              add
                             </span>
                           </div>
+                          <strong className="f1 text-center">
+                            {currency_symbol}
+                            {product.price * product.qty}
+                          </strong>
+                        </div>
+                        <div className={`text-right ${style.loyaltyPoints}`}>
+                          <span>
+                            Earn Loyalty Points: {currency_symbol}2 ?
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={style.footer}>
+                      <div className={style.footerContent}>
+                        <div
+                          className="font-light-black c-pointer d-flex justify-content-center"
+                          onClick={() => handleWishList(product)}
+                          style={{
+                            color: wishlist.find((w) => w.id == product.id)
+                              ? "red"
+                              : "black",
+                          }}
+                        >
+                          <span className={`material-icons-outlined ${style.closeHeart}`}>
+                            {wishlist.find((w) => w.id == product.id)
+                              ? "favorite"
+                              : "favorite_border"}
+                          </span>
+                          <span className="underline">Move to Wishlist</span>
+                        </div>
+                        <Link
+                          to={`/product/${product.sku}`}
+                          className="font-light-black c-pointer d-flex"
+                        >
+                          <span className="material-icons-outlined">
+                            edit
+                          </span>
+                          <span className="underline">Edit</span>
+                        </Link>
+                        <br />
+                        <div className={style.removeBtn}>
+                          <span
+                            className={`material-icons-outlined ${style.close}`}
+                          >
+                            close
+                          </span>
+                          <span
+                            onClick={() => removeProduct(product)}
+                            className={style.remove}
+                          >
+                            Remove
+                          </span>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </Link>
+            </div>
           </>
         ))}
       </div>
