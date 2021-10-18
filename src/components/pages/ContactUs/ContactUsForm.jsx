9a339aca7436436c8b613a810ff4d4dd21117b2e
Dheeraj Varshney <dheerajvarshney@mail.com>
2021-10-18 23:45:20 +0530

    Contact UI

--- src/components/pages/ContactUs/ContactUsForm.jsx
+++ src/components/pages/ContactUs/ContactUsForm.jsx
@@ -1,33 +1,67 @@
-import React from "react";
+
+
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import styles from "./contactus.module.scss";
 
+import {
+  showSnackbar,
+  toggleSignUpCard,
+} from "../../../store/actions/common";
+
+import {
+  addContactEnquiery
+} from "../../../services/contactus/contactus.service";
+
+
 function ContactUsForm() {
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    comment: "",
+    telephone: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+
+  const { email, comment, name, telephone } = formData;
+
+  const userCreateHandler = async (e) => {
+    e.preventDefault();
+    if (!email || !name || !telephone)
+      return dispatch(showSnackbar("All fields are required", "warning"));
+  };
+
   return (
-    <form className = {styles.form}>
+    <form className={styles.form} onSubmit={userCreateHandler}>
       <section className={styles.groupWrap}>
         <div className={styles.formGroup}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" />
+          <input type="text" id="name" name="name" onChange={handleChange} />
         </div>
         <div className={`${styles.formGroup} ${styles.email}`}>
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" />
+          <input type="text" id="email" name="email" onChange={handleChange} />
         </div>
       </section>
       <section className={styles.groupWrap}>
         <div className={styles.formGroup}>
           <label htmlFor="telephone">Telephone</label>
-          <input type="tel" id="telephone" name="telephone" />
+          <input type="tel" id="telephone" name="telephone" onChange={handleChange} />
         </div>
       </section>
       <section className={styles.groupWrap}>
         <div className={styles.formGroup}>
           <label htmlFor="comment">Comment</label>
-          <textarea type="text" id="comment" name="comment" />
+          <textarea type="text" id="comment" name="comment" onChange={handleChange} />
         </div>
       </section>
       <div>
-          <button className={styles.btn} type = "button">Submit</button>
+        <button className={styles.btn} type="button">Submit</button>
       </div>
     </form>
   );
--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import styles from  './contact.module.scss'
+import styles from './contact.module.scss'
 import ContactUsForm from '../../components/pages/ContactUs/ContactUsForm'
 import CustomerService from '../../components/pages/ContactUs/CustomerService'
 
 function ContactUs() {
     return (
-        <div className = {styles.wrap}>
+        <div className={styles.wrap}>
+            <h2 className={styles.title}>CONTACT INFORMATION</h2>
             <ContactUsForm />
-            {/* <CustomerService /> */}
         </div>
     )
 }
