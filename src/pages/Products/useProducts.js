b59f40ac2bcf818b309285c6a6fb9ad803f86d80
satyam.orane <satyam.kumar@oraneconsulting.com>
2022-04-04 12:25:38 +0530

    Filter are updated

--- src/pages/Products/useProducts.js
+++ src/pages/Products/useProducts.js
@@ -27,7 +27,7 @@ const useProducts = ({
       setProducts([]);
       currentPageGet = 1;
     }
-    console.log("filterValue1: ",filterAttr);
+    console.log("filterValue1: ",filterAttr,categoryId,currentPage,pageSize,sortField,sortDirection,onScreen,serachTerm);
     if (filterAttr.status && (filterAttr.newPayloadArr.length > 0)) {
         // filterList(categoryId);
         const keyValue = Object.keys(filterAttr.newPayloadArr[0]);
@@ -35,31 +35,28 @@ const useProducts = ({
         for (let i = 0; i < keyValue.length; i++) {
           console.log("keyValue: ", keyValue[i]);
           if (filterAttr.newPayloadArr[0][keyValue[i]].length > 0) {
-            if (keyValue[i] != "price") {
+            let str = `&filterInfo[filter][${keyValue[i]}]=`;
               for (let j = 0; j < filterAttr.newPayloadArr[0][keyValue[i]].length; j++) {
-           
-                let str = `&searchCriteria[filterGroups][${i+4}][filters][${j}][field]=${keyValue[i]}&searchCriteria[filterGroups][${i+4}][filters][${j}][value]=${filterAttr.newPayloadArr[0][keyValue[i]][j].value}`;
-                filterValue.push(str);
-                filterStr += str;
+                if (j==0) {
+                  str += filterAttr.newPayloadArr[0][keyValue[i]][j].value;
+                } else {
+                  str += `_${filterAttr.newPayloadArr[0][keyValue[i]][j].value}`;
+                }                
               } 
-            } else {
-                let field1 = `&searchCriteria[filterGroups][${i+4}][filters][0][field]=price` ;
-                let miniPrice = `&searchCriteria[filterGroups][${i+4}][filters][0][value]=${filterAttr.newPayloadArr[0][keyValue[i]][0].splitVal[0]}` ;
-                let compare1 = `&searchCriteria[filterGroups][${i+4}][filters][0][condition_type]=from`;
-                let field2 = `&searchCriteria[filterGroups][${i+40}][filters][0][field]=price` ;
-                let maxPrice = `&searchCriteria[filterGroups][${i+40}][filters][0][value]=${filterAttr.newPayloadArr[0][keyValue[i]][filterAttr.newPayloadArr[0][keyValue[i]].length - 1].splitVal[1]}` ;
-                let compare2 = `&searchCriteria[filterGroups][${i+40}][filters][0][condition_type]=to`;                                 
-                  let str = `${field1}${miniPrice}${compare1}${field2}${maxPrice}${compare2}`;
-                  filterStr += str;
-            }
+            filterValue.push(str);
+            filterStr += str;
           }
-                   
         }
-        console.log("filterValue: ",filterAttr, filterStr, keyValue, filterValue);
+        // console.log("filterValue: ",filterAttr, filterStr, keyValue, filterValue);
     }
     let config = {
       method: "get",
-      url: `${API_URL}/products?searchCriteria[filterGroups][0][filters][0][field]=category_id&searchCriteria[filterGroups][1][filters][0][field]=visibility&searchCriteria[filterGroups][1][filters][0][value]=2&searchCriteria[filterGroups][1][filters][1][field]=visibility&searchCriteria[filterGroups][1][filters][1][value]=4&searchCriteria[filterGroups][2][filters][0][field]=status&searchCriteria[filterGroups][2][filters][0][value]=1&searchCriteria[filterGroups][0][filters][0][value]=${categoryId}&searchCriteria[filterGroups][0][filters][0][conditionType]=eq&searchCriteria[sortOrders][0][field]=${sortField}&searchCriteria[sortOrders][0][direction]=${sortDirection}&searchCriteria[pageSize]=${pageSize}&searchCriteria[currentPage]=${currentPageGet}&searchCriteria[filterGroups][3][filters][0][field]=store_id&searchCriteria[filterGroups][3][filters][0][value]=${getStoreId()}${filterStr}`,
+      // url: `${API_URL}/products?searchCriteria[filterGroups][0][filters][0][field]=category_id&searchCriteria[filterGroups][1][filters][0][field]=visibility&searchCriteria[filterGroups][1][filters][0][value]=2&searchCriteria[filterGroups][1][filters][1][field]=visibility&searchCriteria[filterGroups][1][filters][1][value]=4&searchCriteria[filterGroups][2][filters][0][field]=status&searchCriteria[filterGroups][2][filters][0][value]=1&searchCriteria[filterGroups][0][filters][0][value]=${categoryId}&searchCriteria[filterGroups][0][filters][0][conditionType]=eq&searchCriteria[sortOrders][0][field]=${sortField}&searchCriteria[sortOrders][0][direction]=${sortDirection}&searchCriteria[pageSize]=${pageSize}&searchCriteria[currentPage]=${currentPageGet}&searchCriteria[filterGroups][3][filters][0][field]=store_id&searchCriteria[filterGroups][3][filters][0][value]=${getStoreId()}${filterStr}`,
+
+      url: `${API_URL}/webapi/getfilterproducts?filterInfo[category_id]=${categoryId}&filterInfo[pageSize]=${pageSize}&filterInfo[page]=${currentPageGet}&filterInfo[sortOrderField]=position&filterInfo[sortOrder]=ASC&filterInfo[store_id]=${getStoreId()}${filterStr}`,
+
+      // url: `${API_URL}/webapi/getfilterproducts?filterInfo[category_id]=1525&filterInfo[pageSize]=20&filterInfo[page]=1&filterInfo[sortOrderField]=position&filterInfo[sortOrder]=ASC&filterInfo[store_id]=1&filterInfo[filter][size]=117_231&filterInfo[filter][color_swatch]=1727_1722&filterInfo[filter][price]=0-50_50-100`,
+      
       silent: true,
     };
     if (serachTerm) {
